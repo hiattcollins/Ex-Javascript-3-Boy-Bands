376e6a471a2a4983e97d2588c47c12b375731df2
Deaver Hiatt Collins <Hiatt@MacBook-Pro.local>
2017-07-17 17:59:14 -0500

    uploading completed exercise html and js files

--- boybands.js
+++ boybands.js
@@ -11,20 +11,26 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
   
   // Add the band names into the correct <div>
-  currentBand = ???;
+  currentBand = bands[loopTracker];
+  bandElement.innerHTML += `<li>${currentBand}</li>`;
 
+  // console.log(currentBand);
   
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+  currentVeggie = vegetables[loopTracker];
+  veggieElement.innerHTML += `<li>${currentVeggie}</ls>`;
+
+  // console.log(currentVeggie);
+
 
 }
 
